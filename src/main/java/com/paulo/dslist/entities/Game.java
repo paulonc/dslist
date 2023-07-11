45eea06ee365ea13228688a32fee0f154222a6d5
paulonc <paulo.cavalcanti@ccc.ufcg.edu.br>
2023-07-11 18:43:27 -0300

    Criação da classe game

---
+++ src/main/java/com/paulo/dslist/entities/Game.java
@@ -0,0 +1,37 @@
+package com.paulo.dslist.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	
+	@Column(name = "game_year")
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
+
+	public Game() {
+	}
+
+}
